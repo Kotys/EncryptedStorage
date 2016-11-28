9729f412aa8363f83879f7096d36fc1a8ba8c9ba
Jan Kotrba <jan.kotrba@b2a.cz>
2016-11-28 01:02:19 +0100

    LandingPage

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EncryptedCloud\Presenters;
+
+use Nette;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+}
---
+++ app/router/RouterFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace EncryptedCloud;
+
+use Nette;
+use Nette\Application\Routers\RouteList;
+use Nette\Application\Routers\Route;
+
+
+class RouterFactory
+{
+	use Nette\StaticClass;
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public static function createRouter()
+	{
+		$router = new RouteList;
+		$router[] = new Route('', 'Homepage:default');
+		return $router;
+	}
+
+}
